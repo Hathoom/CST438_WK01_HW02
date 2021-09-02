1095b274ace0c78144912bae678bd8b820d8212b
Nicky <43733664+Hathoom@users.noreply.github.com>
2021-09-02 09:20:56 -0700

    Created 2 Edittext fields, I grab input from the first field and pass that input to the LandingActivity.

--- app/src/main/java/com/example/wk01_hw02/MainActivity.java
+++ app/src/main/java/com/example/wk01_hw02/MainActivity.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+
+    private EditText usernameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_input);
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void loginActivity() {
         Intent intent = new Intent(this, LandingActivity.class);
-        String username = "John Doe";
+        String username = usernameInput.getText().toString();
         intent.putExtra("username", username);
         startActivity(intent);
     }
