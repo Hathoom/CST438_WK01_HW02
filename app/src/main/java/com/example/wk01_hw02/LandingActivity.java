6e2b76213954777e61cc21a773e8df847eff5db4
Nicky <43733664+Hathoom@users.noreply.github.com>
2021-09-01 22:40:25 -0700

    Commit of finishing copying from videos.  Created basic intent and example on how to pass data with intent.

--- app/src/main/java/com/example/wk01_hw02/LandingActivity.java
+++ app/src/main/java/com/example/wk01_hw02/LandingActivity.java
@@ -1,14 +1,97 @@
 package com.example.wk01_hw02;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LandingActivity extends AppCompatActivity {
+    private TextView textViewResult;
+
+    private TextView usernameDisplay;
+
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        textViewResult = findViewById(R.id.text_view_result);
+        usernameDisplay = findViewById(R.id.username_display);
+
+        String username = getIntent().getStringExtra("username");
+
+        usernameDisplay.setText(username);
+
+        // will prevent Patch from ignoring null
+        Gson gson = new GsonBuilder().serializeNulls().create();
+
+        //if you add v3 to baseurl has a backslash: /v3/
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        getPosts();
+    }
+
+    private void getPosts() {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("userId", "1");
+        parameters.put("_sort", "id");
+        parameters.put("_order", "desc");
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+
+                List<Post> posts = response.body();
+
+                for (Post post : posts) {
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
     }
 }
